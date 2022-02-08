c0915e3c3dc4f16fcb6f616146827b7ed4a7481f
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-02-08 16:15:15 +0300

    Change class: 03

---
+++ HelloWorld/hello.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "THIS IS THE FIRST COBOL PROGRAM!"
+            STOP RUN.
+       END PROGRAM HELLO-WORLD.
